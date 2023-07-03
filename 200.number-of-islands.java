91151061a7bd0715150801ffc6e3193bfb4b7c01
noahkohrs <noah@kohrs.fr>
2023-07-03 18:10:42 +0200

    little optimisation on 200

--- 200.number-of-islands.java
+++ 200.number-of-islands.java
@@ -9,9 +9,9 @@ class Solution {
     public int numIslands(char[][] grid) {
         Map m = new Map(grid);
         int res = 0;
-        for (int i = 0; i < m.map.length; i++) {
-            for (int j = 0; j < m.map[0].length; j++) {
-                if (m.map[i][j].isLand() && !m.map[i][j].isVisited()) {
+        for (int i = 0; i < m.visited.length; i++) {
+            for (int j = 0; j < m.visited[0].length; j++) {
+                if (m.land[i][j] && !m.visited[i][j]) {
                     m.visit(i, j);
                     res++;
                 }
@@ -21,20 +21,22 @@ class Solution {
     }
 
     class Map {
-        Tile[][] map;
+        boolean[][] land;
+        boolean[][] visited;
 
         Map(char[][] grid) {
-            map = new Tile[grid.length][grid[0].length];
+            land = new boolean[grid.length][grid[0].length];
+            visited = new boolean[grid.length][grid[0].length];
             for (int i = 0; i < grid.length; i++) {
                 for (int j = 0; j < grid[0].length; j++) {
-                    map[i][j] = new Tile(grid[i][j] == '1');
+                    land[i][j] = grid[i][j] == '1';
                 }
             }
         }
 
         void visit(int x, int y) {
-            if (tileExist(x, y) && map[x][y].isLand() && !map[x][y].isVisited()) {
-                map[x][y].visited = true;
+            if (tileExist(x, y) && land[x][y] && !visited[x][y]) {
+                visited[x][y] = true;
                 visit(x + 1, y);
                 visit(x - 1, y);
                 visit(x, y + 1);
@@ -43,25 +45,7 @@ class Solution {
         }
 
         boolean tileExist(int x, int y) {
-            return x >= 0 && x < map.length && y >= 0 && y < map[0].length;
-        }
-
-        class Tile {
-            boolean visited;
-            boolean land;
-
-            Tile(boolean l) {
-                visited = false;
-                land = l;
-            }
-
-            boolean isVisited() {
-                return visited;
-            }
-
-            boolean isLand() {
-                return land;
-            }
+            return x >= 0 && x < land.length && y >= 0 && y < land[0].length;
         }
     }
 }
