ea60ee14f3dca2e67709df29a0191f17b8279caa
noahkohrs <noah@kohrs.fr>
2023-07-03 19:19:01 +0200

    11 Completed (using help from a Solution)

--- 11.container-with-most-water.java
+++ 11.container-with-most-water.java
@@ -6,13 +6,21 @@
 
 // @lc code=start
 
-
 class Solution {
     public int maxArea(int[] height) {
-        int currentMax = 0 ;
-        for (int i = 0 ; i < height.length ; i++) 
-            for (int j = i+1 ; j < height.length ; j++) 
-                currentMax = Math.max(currentMax, Math.min(height[i], height[j]) * (j - i));
+        int left = 0;
+        int right = height.length - 1;
+        int currentMax = 0;
+
+        while (left < right) {
+            currentMax = Math.max(currentMax, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
         return currentMax;
     }
 }
