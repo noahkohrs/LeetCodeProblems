a1e9623d1b2a2cf10877eba57fb8946c4e78a954
noahkohrs <noah@kohrs.fr>
2023-07-03 21:25:37 +0200

    little opti on 18

--- 18.4-sum.java
+++ 18.4-sum.java
@@ -15,20 +15,26 @@ class Solution {
         }
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
             int A = nums[i];
             int B = 0;
             int C = 0;
             int D = 0;
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
 
             for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j - i > 1 && nums[j] == nums[j - 1])
+                    continue;
                 B = nums[j];
                 for (int k = j + 1; k < nums.length - 1; k++) {
+                    if (k - j > 1 && nums[k] == nums[k - 1])
+                        continue;
                     C = nums[k];
                     for (int l = k + 1; l < nums.length; l++) {
+                        if (l - k > 1 && nums[l] == nums[l - 1])
+                            continue;
                         D = nums[l];
-                        if ((long)A + (long)B + (long)C + (long)D == (long)target) {
+                        if ((long) A + (long) B + (long) C + (long) D == (long) target) {
                             List<Integer> temp = new ArrayList<>();
                             temp.add(A);
                             temp.add(B);
