d511f98b57f8b213003337affa386a916d61c77c
noahkohrs <noah@kohrs.fr>
2023-07-07 13:49:08 +0200

    58 done

--- 58.length-of-last-word.java
+++ 58.length-of-last-word.java
@@ -7,7 +7,10 @@
 // @lc code=start
 class Solution {
     public int lengthOfLastWord(String s) {
-        
+        String[] strs = s.split(" ");
+        if (strs.length == 0)
+            return 0;
+        return strs[strs.length - 1].length();
     }
 }
 // @lc code=end
